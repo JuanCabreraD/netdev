3f8038844249f17ca2e2ab9ed157ff356dbdd8f9
JuanCabreraD <120496812+JuanCabreraD@users.noreply.github.com>
2023-02-16 13:54:55 -0500

    DNA Test Upload

---
+++ dnatest.py
@@ -0,0 +1,10 @@
+from dnacentersdk import api
+
+dnac = api.DNACenterAPI(base_url=’https://sandboxdnac2.cisco.com:443’,username=’devnetuser’,password=’Cisco123!’, verify=False)
+
+try:
+devices = dnac.devices.get_device_list()
+for device in devices.response:
+print(device['hostname']," ", device['managementIpAddress'])
+except ApiError as e:
+print(e)
